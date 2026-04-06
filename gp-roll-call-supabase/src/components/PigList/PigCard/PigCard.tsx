9fff4ef4dce90d391b1255556a5d71ac25524c8d
hayleycodes <hello@hayley.codes>
2026-04-06 14:50:06 +0800

    added last sighted to home page

--- gp-roll-call-supabase/src/components/PigList/PigCard/PigCard.tsx
+++ gp-roll-call-supabase/src/components/PigList/PigCard/PigCard.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { formatDistanceToNow } from "date-fns";
 import type { Pig } from "../../../services/pigs.service";
 import "./PigCard.css";
 
@@ -8,10 +9,16 @@ type PigCardProps = {
 };
 
 const PigCard = ({ pig, relationship }: PigCardProps) => {
+  const last_sighted = pig.last_sighted
+    ? formatDistanceToNow(new Date(pig.last_sighted), {
+        addSuffix: true,
+      })
+    : "";
   return (
     <Link to={`/pigs/${pig.id}`} className="pigCard">
       <h3>{pig.name}</h3>
       {relationship ? <span>{relationship}</span> : ""}
+      <span>{last_sighted}</span>
     </Link>
   );
 };
