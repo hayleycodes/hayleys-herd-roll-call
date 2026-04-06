9b6c0a0380a5e1e7a4861177e617a8533b4757fb
hayleycodes <hello@hayley.codes>
2026-04-06 21:30:48 +0800

    Adding things for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  server: {
-    host: true,
-  },
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
       manifest: {
-        name: "GP Roll Call",
-        short_name: "RollCall",
-        theme_color: "#111827",
-        background_color: "#0f172a",
+        name: "Pig Tracker",
+        short_name: "Pigs",
+        description: "Track your pigs 🐷",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
         display: "standalone",
         start_url: "/",
         icons: [
           {
             src: "/pwa-192.png",
             sizes: "192x192",
-            type: "image/png"
+            type: "image/png",
           },
           {
             src: "/pwa-512.png",
             sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
-    })
-  ]
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
