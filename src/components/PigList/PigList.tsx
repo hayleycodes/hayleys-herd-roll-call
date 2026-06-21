e226628ef23c07d9d196d15724648c71fa53f7d9
hayleycodes <hello@hayley.codes>
2026-06-21 15:12:47 +0800

    added resorting  animation

--- src/components/PigList/PigList.tsx
+++ src/components/PigList/PigList.tsx
@@ -176,9 +176,15 @@ const PigList = ({
         {pigs.map((pig, i) => (
           <motion.div
             key={pig.id}
+            layout
             initial={{ opacity: 0, y: 20, scale: 0.95 }}
             animate={{ opacity: 1, y: 0, scale: 1 }}
-            transition={{ delay: i * 0.05, duration: 0.3, ease: 'easeOut' }}
+            transition={{
+              layout: { duration: 0.4, ease: 'easeInOut' },
+              delay: i * 0.05,
+              duration: 0.3,
+              ease: 'easeOut',
+            }}
           >
             <PigCard
               pig={pig}
